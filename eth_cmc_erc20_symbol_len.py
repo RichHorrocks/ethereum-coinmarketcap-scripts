0802a1bbb43349ee9d96fb3b39e4dbb041c65522
Richard Horrocks <richardjhorrocks@gmail.com>
2017-10-27 10:05:47 +0100

    Addition of eth_cmc_erc20_symbol_len.py

---
+++ eth_cmc_erc20_symbol_len.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import urllib, json
+from collections import Counter
+
+url = "https://api.coinmarketcap.com/v1/ticker/"
+response = urllib.urlopen(url)
+data = json.loads(response.read())
+
+counts = Counter([len(asset['symbol']) for asset in data])
+for i, j in sorted(counts.items()):
+    print("Token symbols of length %d: %d" % (i, j))
